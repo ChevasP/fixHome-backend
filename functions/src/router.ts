448a0aa088912a0bb6cfc6dfa8b1514e95faeb3e
Analis12 <anitalis12@gmail.com>
2022-02-09 23:08:59 -0600

    Login

--- functions/src/router.ts
+++ functions/src/router.ts
@@ -1,4 +1,5 @@
 import { Application } from "express";
+import { registro } from "./controllers/autenticacion.controller";
 import { listEstablishment } from "./controllers/establishment_controllers";
 import { listServices } from "./controllers/services_controller";
 import { listUser } from "./controllers/user_controller";
@@ -13,4 +14,8 @@ export function routesServices(app: Application){
 
 export function routesEstablishment(app: Application){
     app.get('/api/establishment', listEstablishment)
+}
+export function routelogin(app: Application){
+    
+    app.post('/api/registro', registro);
 }
